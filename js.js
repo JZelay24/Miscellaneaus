a6c6ee72bfbfd239d94fa811c38ee1ef173951da
JZelay24 <126350495+JZelay24@users.noreply.github.com>
2023-02-25 12:04:52 -0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    
+    const albumText = document.getElementById('albumText');
+
+    albumText.append(' Hola, ' + userName);
+    console.log(userName);
+
+
+
+})
