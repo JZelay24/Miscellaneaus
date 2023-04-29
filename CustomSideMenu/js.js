f9cac002400fb5d3c3e09a21c356ef88e3e00ee9
JZelay24 <126350495+JZelay24@users.noreply.github.com>
2023-04-29 05:59:47 -0600

    Add files via upload

---
+++ CustomSideMenu/js.js
@@ -0,0 +1,12 @@
+const arregloUno = [1,2,3,4]
+const arregloDos = arregloUno.concat([1,2,3])
+
+let promptQuery = prompt("Cual es tu nombre? ")
+
+console.log(arregloDos);
+const header = document.getElementById("header")
+header.innerHTML = ("Halloha")
+
+
+
+header.innerHTML = promptQuery;
